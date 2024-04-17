4c21b7c15a2063e406eaed025a11224227ecdcdd
Tony <ttech@gmx.de>
2024-04-17 06:30:54 +0200

    edit prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://videoflix-backend.tech-mail.eu',
   apiUrl: 'http://127.0.0.1:8000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://127.0.0.1:8000',
-  apiUrl: 'https://videoflix-backend.tech-mail.eu',
+  apiUrl: 'https://videoflix.ttbox.home64.de',
 };
